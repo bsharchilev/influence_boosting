3f10a2310504d172e22a0cbb35141af9f9a41cc5
Boris Sharchilev <bsharchilev@gmail.com>
2018-02-15 17:49:32 +0300

    Fixing env creation script.

--- create_influence_boosting_env.sh
+++ create_influence_boosting_env.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 echo "Preparing influence_boosting conda env..."
 conda create -n influence_boosting python=2.7
 source activate influence_boosting
-pip install numpy scipy pandas scikit-learn matplotlib
+pip install numpy==1.14.0 scipy==0.19.1 pandas==0.20.3 scikit-learn==0.19.0 matplotlib==2.0.2 tensorflow==1.6.0rc0 tqdm==4.19.5
